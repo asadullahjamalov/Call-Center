3fe3514fc95d0d73161d0b40b04f8b943bc32cee
asadullahjamalov <asadullahnj@code.edu.az>
2021-10-03 23:05:31 +0400

    add spring security

---
+++ src/main/java/com/example/callcenterproject/CallCenterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.callcenterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallCenterProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/callcenterproject/model/JwtResponse.java
@@ -0,0 +1,11 @@
+package com.example.callcenterproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class JwtResponse {
+    private final String token;
+}
